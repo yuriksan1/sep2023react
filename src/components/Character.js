29902f5362fa9e25913eb5151d4ba07443a75dc3
yuriksan1 <yurakomow2@gmail.com>
2024-02-03 19:08:17 +0200

    lesson1

---
+++ src/components/Character.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Character = ({character}) => {
+    const {name,img} = character;
+    return (
+        <div>
+            <h1>{name}</h1>
+            <img
+                src={img}
+                alt={name}/>
+        </div>
+    );
+};
+
+export {Character};
